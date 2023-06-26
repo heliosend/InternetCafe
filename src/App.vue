8a4d1c33203ffe652733b90445885f32b7faa1d1
heliosend <108450943+heliosend@users.noreply.github.com>
2023-06-26 21:51:56 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <index></index>>
+  </div>
+</template>
+
+<script>
+
+import Index from "./components/Index"
+
+export default {
+  name: 'App',
+  components: {
+  Index
+  }
+}
+</script>
+
+<style>
+#app {
+  height :100%;
+}
+</style>
---
+++ src/components/Index.vue
@@ -0,0 +1,45 @@
+<template>
+  <el-container style="height: 100%; border: 1px solid #eee">
+    <el-aside width="200px" style="background-color: rgb(238, 241, 246);height:100%">
+      <Aside></Aside>
+    </el-aside>
+
+    <el-container style="height: 100%;">
+      <el-header style="text-align: right; font-size: 12px;height: 100%">
+        <Header></Header>
+      </el-header>
+
+      <el-main style="height: 100%;">
+        <Main></Main>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+import Aside from "@/components/Aside.vue";
+import Header from "@/components/Header.vue";
+import Main from "@/components/Main.vue";
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Index",
+  components: {Header, Aside, Main},
+}
+</script>
+
+<style>
+.el-header {
+  background-color: #B3C0D1;
+  color: #333;
+  line-height: 60px;
+}
+
+.el-main {
+  padding: 5px;
+}
+
+.el-aside {
+  color: #333;
+}
+</style>
